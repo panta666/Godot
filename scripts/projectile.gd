44bcf36e6d0d98e4fcbb3f4dc776780135eddc1f
Sebwen <zentrum164@gmail.com>
2025-11-17 16:13:14 +0100

    Fernangriffe

---
+++ scripts/projectile.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+@export var speed: float = 400.0       # Pixels per second
+@export var direction: Vector2 = Vector2.RIGHT  # Direction the projectile moves
+
+
+func _physics_process(delta):
+	# Move the projectile
+	position += direction.normalized() * speed * delta
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
