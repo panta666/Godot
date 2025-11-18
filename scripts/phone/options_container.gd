6613b91ad06eefcd1a8d9c13cc45cc849e83c8e3
panta666 <panta666@web.de>
2025-11-18 22:30:43 +0100

    Add Scene for Option Container

---
+++ scripts/phone/options_container.gd
@@ -0,0 +1,40 @@
+extends CenterContainer
+
+# --- Options-UI ---
+@onready var master_volume: HSlider = $OptionsContainer/VBoxContainer/MasterVolume
+@onready var check_box: CheckBox = $OptionsContainer/VBoxContainer/CheckBox
+@onready var music_volume: HSlider = $OptionsContainer/VBoxContainer/MusicVolume
+@onready var sfx_volume: HSlider = $OptionsContainer/VBoxContainer/SFXVolume
+
+# --- Soundbus-IDs ---
+const MASTER_BUS := 0
+const MUSIC_BUS := 1
+const SFX_BUS := 2
+
+
+# --------------------------
+# Audio
+func _on_master_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(MASTER_BUS, value)
+
+func _on_music_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(MUSIC_BUS, value)
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(MASTER_BUS, toggled_on)
+	SaveManager.update_is_muted(toggled_on)
+
+
+func _on_master_volume_drag_ended(_value_changed: bool) -> void:
+	SaveManager.update_bus_volume(MASTER_BUS, master_volume.value)
+
+
+func _on_music_volume_drag_ended(_value_changed: bool) -> void:
+	SaveManager.update_bus_volume(MUSIC_BUS, music_volume.value)
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(SFX_BUS, value)
+
+
+func _on_sfx_volume_drag_ended(_value_changed: bool) -> void:
+	SaveManager.update_bus_volume(SFX_BUS, sfx_volume.value)
