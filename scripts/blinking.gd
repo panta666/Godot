3a94357476e2be54ba01cf64326fe5d7919a3b25
panta666 <panta666@web.de>
2025-11-20 21:21:36 +0100

    Added blink speed option to set_blink

--- scripts/blinking.gd
+++ scripts/blinking.gd
@@ -3,5 +3,7 @@ extends Node2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
-func blinking(state: bool):
+func set_blinking_on(state: bool, speed := 0):
 	sprite_2d.material.set_shader_parameter('blink_toggle', state)
+	if (speed > 0):
+		sprite_2d.material.set_shader_parameter('blink_speed', speed)
