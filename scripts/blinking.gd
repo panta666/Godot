d35b0d53a01bfe31f85c8d04716e2ced32e6f896
panta666 <panta666@web.de>
2025-11-20 22:40:46 +0100

    add tutorial controll to disable blinking

--- scripts/blinking.gd
+++ scripts/blinking.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
+func _ready() -> void:
+	self.visible = GlobalScript.tutorial_on
+
+
 func set_blinking_on(state: bool, speed := 0):
 	sprite_2d.material.set_shader_parameter('blink_toggle', state)
 	if (speed > 0):
