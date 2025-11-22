3689ddf345525ee965279fcb834a43aaedce3533
Panata666 <panta666@web.de>
2025-11-22 20:20:33 +0100

    change standard resulution to fit in res array. implement Res change. Reposition Tablet in Main Menu. Add Fullscreesn support

--- scripts/global.gd
+++ scripts/global.gd
@@ -43,6 +43,21 @@ var tutorial_on := true
 signal tutorial_toggled(is_enabled: bool)
 
 # -------------------------
+# Possible Resolutions for the Game (global)
+# -------------------------
+var resolutions = {
+	"3840x2160": Vector2i(3840, 2160),
+	"2560x1440": Vector2i(2560, 1440),
+	"1920x1080": Vector2i(1920, 1080),
+	"1366x768": Vector2i(1366, 768),
+	"1280x720": Vector2i(1280, 720),
+	"1440x900": Vector2i(1440, 900),
+	"1600x900": Vector2i(1600, 900),
+	"1024x600": Vector2i(1024, 600),
+	"800x600": Vector2i(800, 600)
+}
+
+# -------------------------
 # Neues Spiel starten
 # -------------------------
 func start_new_game() -> void:
