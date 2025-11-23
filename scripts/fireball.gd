7feeb493aa74fd43f021e0cb23491e440ff383f3
Dave <david-otten453@web.de>
2025-11-23 18:19:51 +0100

    Projektil ist jetzt eine Kapsel

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -6,17 +6,61 @@ var direction: float
 @onready var fireball_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 
+var current_scene
+var scene_name
+
+var is_dead = false
+
+func _ready():
+	current_scene = get_tree().current_scene
+	scene_name = current_scene.name
+	
+	change_size()
+
 func _physics_process(delta: float) -> void:
-	#Sprite-Flip
-	fireball_sprite.flip_h = (direction > 0)
 	move_local_x(direction * SPEED * delta)
 	
+	play_run_animation()
+	
 	if ray_cast_2d.is_colliding(): #Collision mit Wänden
-		fireball_sprite.play("death")
-		await fireball_sprite.animation_finished
-		queue_free()
+		play_death_animation()
 
 func _on_hit_box_area_entered(_area: Area2D) -> void: #Collision mit Gegnern
-	fireball_sprite.play("death")
+	play_death_animation()
+
+func play_run_animation():
+	if is_dead == true:
+		return
+	if "Level_one" in scene_name:
+		fireball_sprite.play("oop_run")
+	elif "medg" in scene_name:
+		#fireball_sprite.play("medg_run")
+		pass
+	else:
+		fireball_sprite.play("run")
+
+func change_size():
+	if "Level_one" in scene_name:
+		fireball_sprite.flip_h = (direction < 0)
+		fireball_sprite.scale = Vector2(0.094,0.154)
+		fireball_sprite.position = Vector2(-20, -1)
+	elif "medg" in scene_name:
+		fireball_sprite.flip_h = (direction < 0)
+		fireball_sprite.scale = Vector2(1,1)
+		fireball_sprite.position = Vector2(0, 0)
+	else:
+		fireball_sprite.flip_h = (direction < 0)
+		fireball_sprite.scale = Vector2(1, 1)
+		fireball_sprite.position = Vector2(0, 0)
+
+func play_death_animation():
+	is_dead = true
+	if "Level_one" in scene_name:
+		fireball_sprite.play("oop_death")
+	elif "medg" in scene_name:
+		#fireball_sprite.play("medg_death")
+		pass
+	else:
+		fireball_sprite.play("death")
 	await fireball_sprite.animation_finished
 	queue_free()
--- scripts/range_attack_charges.gd
+++ scripts/range_attack_charges.gd
@@ -5,10 +5,19 @@ extends TextureButton
 
 var max_value := 100.0  # ProgressBar max = 100
 
+#Verschiedene Angriffe Preloaden
+const ATTACK_ICONS  ={
+	"oop_capsule": preload("res://assets/range_attacks/oop_capsule/oop_capsule.png"),
+	"fireball": preload("res://assets/fire ball/fire ball 1.png")
+}
+
+
 func _ready():
 	charge_bar.min_value = 0
 	charge_bar.max_value = max_value
 	charge_bar.value = charge_bar.min_value  # Start: voll
+	
+	set_attack_texture()
 
 func update_charge_text(current: int, max_charges: int):
 	current_charges.text = "%d / %d" % [current, max_charges]
@@ -16,3 +25,14 @@ func update_charge_text(current: int, max_charges: int):
 # progress zwischen 0.0 → 1.0 umgerechnet auf 0 → 100
 func update_recharge_progress(progress: float):
 	charge_bar.value = max_value - progress * max_value
+
+func set_attack_texture():
+	var current_scene = get_tree().current_scene
+	var scene_name = current_scene.name
+	print(scene_name)
+	if "oop" in scene_name:
+		texture_normal = ATTACK_ICONS["oop_capsule"]
+	elif "medg" in scene_name:
+		texture_normal = ATTACK_ICONS["fireball"]
+	else:
+		texture_normal = ATTACK_ICONS["fireball"]
