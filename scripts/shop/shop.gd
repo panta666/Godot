ad563fb0a9ee1a77ae7bcbd2221e2cf600311481
Dennis Strutmann <dstrutmann@hotmail.de>
2025-12-11 15:45:33 +0100

    shop core interface implemented

---
+++ scripts/shop/shop.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+@export var shop_items: Array[ShopData] = []
+
+@onready var interactable: Area2D = $Interactable
+@onready var shop_ui: CanvasLayer = $ShopUI
+
+var shop_open := false
+
+func _ready() -> void:
+	# Interaktion registrieren
+	interactable.interact = _on_interact
+	interactable.is_interactable = true
+
+
+func _process(_delta: float) -> void:
+	# UI Text setzen
+	if interactable.is_interactable:
+		if shop_open:
+			interactable.interact_name = ""
+		else:
+			interactable.interact_name = "to open the shop"
+
+
+func _on_interact() -> void:
+	var player = GlobalScript.player
+	if not player:
+		return
+	# ------------------------------------------
+	# Shop öffnen
+	# ------------------------------------------
+	if not player.is_shopping:
+		player.open_shop()
+		interactable.is_interactable = false
+
+		# ShopUI erscheint
+		shop_ui.show()
+		get_node("ShopUI/AnimationPlayer").play("transition_in")
+
+	# ------------------------------------------
+	# Shop schließen
+	# ------------------------------------------
+	else:
+		player.close_shop()
+		interactable.is_interactable = true
+
+		# ShopUI schließt sich
+		get_node("ShopUI/AnimationPlayer").play("transition_out")
---
+++ scripts/shop/shop_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name ShopData
+@export var item_name: String
+@export var icon: Texture2D
+@export var price: int
+@export var powerup_name: String   # z.B. "double_jump", "dash", "range_attack"
