0618127a85a2e2aa9edc8d4f14688575e1476460
panta666 <panta666@web.de>
2025-12-14 14:06:24 +0100

    remove debug print

--- scripts/hud/dreamworld/number_of_coins_label.gd
+++ scripts/hud/dreamworld/number_of_coins_label.gd
@@ -6,6 +6,5 @@ func _ready() -> void:
 	GlobalScript.coin_collected.connect(update_coins)
 
 func update_coins(_level_name:String, value: int):
-	print("update coins: " + str(value))
 	coins = value
 	text = "Coins: " + str(value)
