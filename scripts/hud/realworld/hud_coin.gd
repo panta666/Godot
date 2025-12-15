04fce60798d55db730a8b99867f205e9bc1d92de
panta666 <panta666@web.de>
2025-12-15 20:12:14 +0100

    rework hud logic

--- scripts/hud/realworld/hud_coin.gd
+++ scripts/hud/realworld/hud_coin.gd
@@ -1,8 +1,11 @@
 extends Control
 
 var coins = 0
-@onready var number_of_coins: Label = $NumberOfCoins
+@onready var number_of_coins_label: Label = $"."
+
+func _ready() -> void:
+	GlobalScript.realworld_coins_update.connect(update_coins)
 
 func update_coins(value: int):
 	coins = value
-	number_of_coins.text = "Coins: " + str(value)
+	number_of_coins_label.text = "Coins: " + str(value)
