24d359cbcf98d4ce710b6ffef5978360b37fb30d
Dennis Strutmann <dstrutmann@hotmail.de>
2025-12-31 14:00:24 +0100

    In shop_data AnimatedSprite2D in Texture2D geaendert

--- src/features/shop/shop_data.gd
+++ src/features/shop/shop_data.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name ShopData
 @export var item_name: String
-@export var sprite_frames: SpriteFrames
+@export var texture: Texture2D
 @export var price: int
 @export var description: String
 @export var powerup_name: String   # z.B. "double_jump", "dash", "range_attack"
