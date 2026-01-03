fbb644d395b77ff72b97fc2b83e75bf60dd4bc00
Sebwen <zentrum164@gmail.com>
2026-01-03 19:37:55 +0100

    Boss Health-System

--- src/features/enemies/boss_enemy.gd
+++ src/features/enemies/boss_enemy.gd
@@ -5,12 +5,33 @@ class_name OOP_Boss
 
 @export var boss_trigger: Boss_Trigger
 
+var bar_instance
+
+var lifes := 3
+
+var boss_bar = preload("res://src/features/enemies/boss_healthbar.tscn")
+
 func _ready() -> void:
-	call_deferred("_spawn_healthbar")
+	pass
+
+func _spawn_healthbar() -> void:
+	print("spawned healthbar")
+	bar_instance = boss_bar.instantiate()
+	get_tree().root.add_child(bar_instance)
+	bar_instance.setup(self)
 
 func _get_visible_player() -> CharacterBody2D:
 	return boss_trigger._get_player()
 	
+func _detect_player() -> bool:
+	var collider := _get_visible_player()
+	if collider:
+		if bar_instance == null:
+			_spawn_healthbar()
+		player = collider
+		return true
+	return false
+
 func _handle_player_logic() -> void:
 	if _detect_player():
 		is_walking = true
@@ -19,38 +40,43 @@ func _handle_player_logic() -> void:
 		if not is_dashing:
 			_track_player()
 	else:
+		if bar_instance != null:
+			bar_instance.queue_free()
+			bar_instance = null
 		player = null
 		is_walking = true
 		sprite.play("walk")
 		sound_player.play_sound(Enemysound.soundtype.WALK)
 
-func _on_health_depleted() -> void:
-	healthbar._deplete()
-	print("Level One Bereich betreten! OOP Level 2 freischalten.")
 
-	# Level 2 für OOP freischalten
-	GlobalScript.unlock_level(GlobalScript.classrooms.oop, 2)
-	#Shop freischalten in Realworld
-	SaveManager.unlock_shop()
-	
-	# TEST MATH Room freischalten
-#----------------------------------------------------------------
-	# BITTE ENTFERNEN UND IN BOSS FÜR LEVEL 2 HINZUFÜGEN
-	SaveManager.unlock_door("realworld_math_door")
-#----------------------------------------------------------------
+func die() -> void:
+	if lifes <= 0:
+		bar_instance._deplete()
+		print("Level One Bereich betreten! OOP Level 2 freischalten.")
 
-	# LevelUI aktualisieren, falls vorhanden
-	var classroom = get_tree().current_scene
-	if classroom.has_node("LevelUI"):
-		var level_ui = classroom.get_node("LevelUI") as CanvasLayer
-		# level_ui muss die unlock Funktion oder update_level_button nutzen
-		if "unlock_oop_level" in level_ui:
-			level_ui.unlock_oop_level(1)
-		else:
-			level_ui.update_level_button()
-			
-	await _return_to_classroom()
-	queue_free()
+		# Level 2 für OOP freischalten
+		GlobalScript.unlock_level(GlobalScript.classrooms.oop, 2)
+		#Shop freischalten in Realworld
+		SaveManager.unlock_shop()
+
+		# TEST MATH Room freischalten
+	#----------------------------------------------------------------
+		# BITTE ENTFERNEN UND IN BOSS FÜR LEVEL 2 HINZUFÜGEN
+		SaveManager.unlock_door("realworld_math_door")
+	#----------------------------------------------------------------
+
+		# LevelUI aktualisieren, falls vorhanden
+		var classroom = get_tree().current_scene
+		if classroom.has_node("LevelUI"):
+			var level_ui = classroom.get_node("LevelUI") as CanvasLayer
+			# level_ui muss die unlock Funktion oder update_level_button nutzen
+			if "unlock_oop_level" in level_ui:
+				level_ui.unlock_oop_level(1)
+			else:
+				level_ui.update_level_button()
+
+		await _return_to_classroom()
+		queue_free()
 
 
 func _return_to_classroom() -> void:
@@ -62,17 +88,34 @@ func _return_to_classroom() -> void:
 	GlobalScript.change_scene("realworld_classroom_one")
 
 func _on_hurt_box_received_damage(damage: int, _attacker_pos: Vector2) -> void:
-	healthbar.update()
+	if bar_instance != null:
+		bar_instance.update()
 	damaged.emit(damage)
 	GlobalScript.enemy_damaged.emit(damage)
 	flash_anim.play("flash")
 
 
 func _on_hurt_box_torso_received_damage(damage: int, attacker_position: Vector2) -> void:
-	healthbar.update()
+	if bar_instance != null:
+		bar_instance.update()
 	flash_anim.play("flash")
 
 
 func _on_hurt_box_legs_received_damage(damage: int, attacker_position: Vector2) -> void:
-	healthbar.update()
+	if bar_instance != null:
+		bar_instance.update()
 	flash_anim.play("flash")
+
+func _on_health_depleted() -> void:
+	lifes -= 1
+	die()
+
+func _on_health_torso_health_depleted() -> void:
+	lifes -= 1
+	die()
+
+
+func _on_health_legs_health_depleted() -> void:
+	lifes -= 1
+	print("lifes: ", lifes)
+	die()
---
+++ src/features/enemies/boss_healthbar.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+@onready var health_bar = $Control/ProgressBar
+
+@onready var health_bar2 = $Control/ProgressBar2
+
+@onready var health_bar3 = $Control/ProgressBar3
+
+var health_nodes = []
+
+var boss
+
+func setup(_boss: Enemy):
+	boss = _boss
+	for node in boss.get_tree().get_nodes_in_group("health"):
+		health_nodes.append(node)
+		
+	print(health_nodes)
+	update()
+
+
+func update():
+	for node in health_nodes:
+		if node == null:
+			return
+	health_bar.value = health_nodes[0].get_health() * 100 / health_nodes[0].get_max_health()
+	health_bar2.value = health_nodes[1].get_health() * 100 / health_nodes[1].get_max_health()
+	health_bar3.value = health_nodes[2].get_health() * 100 / health_nodes[2].get_max_health()
+
+		
+func _deplete():
+	queue_free()
