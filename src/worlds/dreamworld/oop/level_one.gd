3c1222e495a14770a9cb020ac3f8c379800ee70c
Dennis Strutmann <dstrutmann@hotmail.de>
2026-01-07 15:53:21 +0100

    key_fragment dialog added in oop level one

--- src/worlds/dreamworld/oop/level_one.gd
+++ src/worlds/dreamworld/oop/level_one.gd
@@ -1,39 +1,50 @@
 extends Node2D
 
 @export var timeline_1: String = "level_one_timeline"
+@export var timeline_2: String = "level_one_keys_timeline"
 @onready var dialog_trigger: Area2D = $DialogTrigger
+@onready var dialog_trigger2: Area2D = $DialogTrigger2
 
-var cutscene_played := false
+var cutscene1_played := false
+var cutscene2_played := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.signal_event.connect(Callable(self, "_on_dialogic_signal"))
 
-	dialog_trigger.body_entered.connect(_on_dialog_trigger_entered)
+	dialog_trigger.body_entered.connect(_on_dialog_trigger_entered_timeline1)
+	dialog_trigger2.body_entered.connect(_on_dialog_trigger_entered_timeline2)
 	# Speichert die aktuelle Szene für continue
 	SaveManager.update_current_scene()
 	MusicManager.playMusic(MusicManager.MusicType.DREAMWORLD)
 
-func _on_dialog_trigger_entered(body: Node) -> void:
-	# 1. Ist der Body selbst der Player?
+func _on_dialog_trigger_entered_timeline1(body: Node) -> void:
+	if is_player(body):
+		start_cutscene_with(body, timeline_1)
+
+func _on_dialog_trigger_entered_timeline2(body: Node) -> void:
+	if is_player(body):
+		start_cutscene_with(body, timeline_2)
+
+func start_cutscene_with(player: Node, timeline_name: String):
+	match timeline_name:
+		timeline_1:
+			if cutscene1_played:
+				return
+			cutscene1_played = true
+		timeline_2:
+			if cutscene2_played:
+				return
+			cutscene2_played = true
+
+	_start_cutscene(player, timeline_name)
+
+func is_player(body: Node) -> bool:
 	if body.has_method("player"):
-		start_cutscene_with(body)
-		return
-
-	# 2. Ist der Parent des Bodys der Player?
+		return true
 	if body.get_parent() and body.get_parent().has_method("player"):
-		start_cutscene_with(body.get_parent())
-		return
-
-	# 3. Sonst ignorieren
-	print("Kein Player:", body.name)
-
-func start_cutscene_with(player: Node):
-	if cutscene_played:
-		return
-	_start_cutscene(player, timeline_1)
-	cutscene_played = true
-
+		return true
+	return false
 
 func _start_cutscene(body: Node, timeline_name: String) -> void:
 	print("Versuche Cutscene zu starten:", timeline_name, "für", body.name)
