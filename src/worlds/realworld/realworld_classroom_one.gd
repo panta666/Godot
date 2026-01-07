b4540dfdefa3ac322643bd325d7b57961d337129
Dennis Strutmann <dstrutmann@hotmail.de>
2026-01-07 18:58:03 +0100

    boss trigger for new quest dialog

--- src/worlds/realworld/realworld_classroom_one.gd
+++ src/worlds/realworld/realworld_classroom_one.gd
@@ -1,6 +1,6 @@
 extends RealworldScenes
 @onready var classroom_ambiance_player: AudioStreamPlayer = $SFX/ClassroomAmbiancePlayer
-
+@onready var quest_trigger2: Area2D = $QuestTrigger2
 
 func _ready() -> void:
 	classroom_ambiance_player.play()
@@ -9,6 +9,19 @@ func _ready() -> void:
 	previous_scene_spawn = Vector2(568, 362) # Position, wenn aus Hall kommt
 	super._ready()  # Basisklasse _ready aufrufen
 	Dialogic.signal_event.connect(_on_dialogic_signal)
+	
+	# QuestTrigger2 standardmäßig deaktivieren
+	var trigger := $QuestTrigger2
+	trigger.monitoring = false
+	trigger.monitorable = false
+	trigger.get_node("CollisionShape2D").disabled = true
+
+	# Prüfen, ob Level OOP Level 2 freigeschaltet ist
+	if GlobalScript.is_level_unlocked(GlobalScript.classrooms.oop, 2):
+		trigger.monitoring = true
+		trigger.monitorable = true
+		trigger.get_node("CollisionShape2D").disabled = false
+		print("QuestTrigger2 aktiviert, OOP Level 2 freigeschaltet!")
 
 func _on_dialogic_signal(event_name: String) -> void:
 	# Cutscene freigeben
