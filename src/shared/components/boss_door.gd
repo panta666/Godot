49bdf595ea45c56e6a3243183ba0653f6c4a83de
panta666 <panta666@web.de>
2026-01-12 10:34:28 +0100

    Open Bossdoor in profmode

--- src/shared/components/boss_door.gd
+++ src/shared/components/boss_door.gd
@@ -4,6 +4,10 @@ extends StaticBody2D
 @export var required_keys := 4
 var is_open = false
 
+func _ready():
+	if GlobalScript.is_prof_mode():
+		open_door()
+
 func open_door():
 	is_open = true
 	sprite_2d.visible = false
