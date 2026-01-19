e5803c133af96c6e755a339bcf9f1bd9724a3fc7
Sh1non02 <jonas-holzem@gmx.de>
2026-01-19 03:59:57 +0100

    small lvl changes and code clean up

--- src/shared/components/boss_door.gd
+++ src/shared/components/boss_door.gd
@@ -1,14 +1,14 @@
 extends StaticBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@export var required_keys := 4
 var is_open = false
 
-#Öffnet die Tür zum Bossraum
+#Öffnet die Tür bei aktiviertem Bossmode
 func _ready():
 	if GlobalScript.is_prof_mode():
 		open_door()
 
+#Öffnet die Tür zum Bossraum
 func open_door():
 	is_open = true
 	sprite_2d.visible = false
